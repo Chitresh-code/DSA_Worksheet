b82a0f564ce8b104afecf06c067eb0238bcb7172
Prabhat <prabhatdwivedi224@gmail.com>
2023-10-21 01:22:15 +0530

    Code for insertion sort in C added

---
+++ DSA_Codesheet/C/insertionsort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    
+
+    return 0;
+}
