73efc215ac86c99263c8a8e11c3ef55ea17998ab
rohit <rohitsharma4102002@gmail.com>
2023-10-27 09:01:56 +0530

    added counting sort c++

--- DSA_Codesheet/Cpp/countSort.cpp
+++ DSA_Codesheet/Cpp/countSort.cpp
@@ -1,66 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-void countingSort(vector<int> &arr)
-{
-    int max = *max_element(arr.begin(), arr.end()); // Find the maximum element in the array
+vector<int> sortArray(vector<int>& N) {
+    int min_val = *min_element(N.begin(), N.end());
+    int max_val = *max_element(N.begin(), N.end());
 
-    vector<int> count(max + 1, 0); // Create a count array to store the count of each element
-    vector<int> output(arr.size());
+    vector<int> count(max_val - min_val + 1, 0);
 
-    // Count the occurrences of each element in the input array
-    for (int i = 0; i < arr.size(); ++i)
-    {
-        count[arr[i]]++;
+    for (int num : N) {
+        count[num - min_val]++;
     }
 
-    // Calculate the cumulative count
-    for (int i = 1; i <= max; ++i)
-    {
-        count[i] += count[i - 1];
+    vector<int> sorted_array;
+    for (size_t i = 0; i < count.size(); i++) {
+        for (int j = 0; j < count[i]; j++) {
+            sorted_array.push_back(i + min_val);
+        }
     }
 
-    // Build the output array
-    for (int i = arr.size() - 1; i >= 0; --i)
-    {
-        output[count[arr[i]] - 1] = arr[i];
-        count[arr[i]]--;
-    }
-
-    // Copy the sorted elements back to the original array
-    for (int i = 0; i < arr.size(); ++i)
-    {
-        arr[i] = output[i];
-    }
+    return sorted_array;
 }
 
-int main()
-{
+int main() {
     int n;
     cout << "Enter the number of elements: ";
     cin >> n;
 
-    vector<int> arr(n);
-    cout << "Enter " << n << " positive elements: ";
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> arr[i];
+    vector<int> user_input(n);
+    cout << "Enter the elements separated by spaces: ";
+    for (int i = 0; i < n; i++) {
+        cin >> user_input[i];
     }
-    cout << "array before sort: ";
-    for (int num : arr)
-    {
-        cout << num << " ";
-    }
-    cout << endl;
-    countingSort(arr);
 
-    cout << "array after sort: ";
-    for (int num : arr)
-    {
+    // Output the array before sorting
+    cout << "Array Before Sorting :-" << endl;
+    for (int num : user_input) {
         cout << num << " ";
     }
     cout << endl;
+
+    // Sort the array
+    vector<int> sorted_list = sortArray(user_input);
+
+    // Output the array after sorting
+    cout << "Array After Sorting :-" << endl;
+    for (int num : sorted_list) {
+        cout << num << " ";
+    }
+    cout << endl;
+
     return 0;
 }
