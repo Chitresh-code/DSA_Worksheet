b546d38071f8fcb781e4f8d2600b3d0adf4db9d0
Akshath Mangudi <akshathmangudi@gmail.com>
2023-10-28 21:43:44 +0530

    Updated triplet_sum.cpp

--- DSA_Codesheet/Cpp/triplet_sum.cpp
+++ DSA_Codesheet/Cpp/triplet_sum.cpp
@@ -1,30 +1,28 @@
 #include <bits/stdc++.h>
 
-using namespace std;
+using namespace std; 
 
-void findTriplet(int A[], int arr_size, int sum) {
+bool find3Numbers(int A[], int arr_size, int sum) {
 	for (int i = 0; i < arr_size - 2; i++) {
-		// A[i] == first elem
-		for (int j = i + 1; i < arr_size - 1; j++) {
-			// A[j] == second elem
-			for (int k = j + 1; k < arr_size; k++) {
-				// A[k] == third elem
+		for (int j = 1; j < arr_size - 1; j++) {
+			for (int k = 1; k < arr_size; k++) {
 				if (A[i] + A[j] + A[k] == sum) {
-					cout << "Triplet satisfied. Numbers are " <<
-						A[i] << ", " << A[j] << "and " << A[k];
+					cout << "Triplet is " << A[i] <<
+						", " << A[j] << ", " << A[k]; 
+					return true; 
 				}
 			}
 		}
 	}
-	cout << "Triplet not satisfied."; 
+
+	return false; 
 }
 
 int main()
 {
-	int A[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; 
-	int sum = 55; 
-	int arr_size = sizeof(A) / sizeof(A[0]);
-	findTriplet(A, arr_size, sum); 
+	int A[] = { 1, 2, 4, 67, 23, 89, 6, 8, 10 }; 
+	int sum = 32; 
+	int arr_size = sizeof(A) / sizeof(A[0]); 
+	find3Numbers(A, arr_size, sum); 
 	return 0;
 }
-
